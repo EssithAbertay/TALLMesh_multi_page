52ab31b20a64a65eb1fd41dbb47904a4367f4f97
TG Stromboli <alexjfawzi@gmail.com>
2024-10-23 15:02:11 +0100

    using st.plotly_chart with sankey

--- pages/15_⚖️_Data_Ethics.py
+++ pages/15_⚖️_Data_Ethics.py
@@ -9,3 +9,13 @@ import shutil
 # Set logo
 logo = "pages/static/tmeshlogo.png"
 st.logo(logo)
+
+def main(): 
+
+    # we should put the info here
+
+    # Manage API keys
+    manage_api_keys()
+
+if __name__ == "__main__":
+    main()
