a00ba2168689d41faee656be3408920280c9f8f2
TG Stromboli <alexjfawzi@gmail.com>
2024-10-24 14:27:35 +0100

    Unassigned codes returned separately by default

--- pages/16_👨‍👩‍👦‍👦_Community.py
+++ pages/16_👨‍👩‍👦‍👦_Community.py
@@ -5,7 +5,22 @@ import json
 from api_key_management import manage_api_keys
 import shutil
 
-
 # Set logo
 logo = "pages/static/tmeshlogo.png"
 st.logo(logo)
+
+def main():
+
+    # Just some placeholder for now until public
+
+    # Display logo
+    logo = "pages/static/tmeshlogo.png"
+    st.image(logo)
+
+    st.header(("Community Details Coming Soon!!"))
+
+    manage_api_keys()
+
+
+if __name__ == "__main__":
+    main()
